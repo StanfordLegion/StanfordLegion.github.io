ebd2e00cb53713b1908877a50c82ea5e6a0260d9
Elliott Slaughter <elliottslaughter@gmail.com>
2017-08-29 16:32:59 -0700

    Deploy Tue Aug 29 16:32:56 PDT 2017.

--- doxygen/search/functions_8.js
+++ doxygen/search/functions_8.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['is_5fempty',['is_empty',['../class_legion_1_1_future.html#a1a686a2469666cab55ee3f52848952ee',1,'Legion::Future']]],
-  ['is_5fexclusive',['is_exclusive',['../class_legion_1_1_projection_functor.html#a841eefa7a1a992b491dd7b6564a18ed5',1,'Legion::ProjectionFunctor']]],
+  ['is_5fempty',['is_empty',['../class_legion_1_1_future.html#a75721ea98fd87fc454f657d5651407e0',1,'Legion::Future']]],
+  ['is_5fexclusive',['is_exclusive',['../class_legion_1_1_projection_functor.html#a882284d677bc97fba11e93bf6a6fd1e0',1,'Legion::ProjectionFunctor']]],
   ['is_5findex_5fpartition_5fcomplete',['is_index_partition_complete',['../class_legion_1_1_runtime.html#a36af197f72cc33ba83d0ada197002bb5',1,'Legion::Runtime']]],
   ['is_5findex_5fpartition_5fdisjoint',['is_index_partition_disjoint',['../class_legion_1_1_runtime.html#a2d5acf558aa75f074a816c9e897a9777',1,'Legion::Runtime']]],
-  ['is_5fmapped',['is_mapped',['../class_legion_1_1_physical_region.html#a64e75c87cfcede6ab921f4e05582a89c',1,'Legion::PhysicalRegion']]],
-  ['is_5fvalid',['is_valid',['../class_legion_1_1_physical_region.html#a7f760ac53aa8535bf2bf1cf03eab14de',1,'Legion::PhysicalRegion']]],
+  ['is_5fmapped',['is_mapped',['../class_legion_1_1_physical_region.html#a969f7ab495c5488123b98f9e8e981f65',1,'Legion::PhysicalRegion']]],
+  ['is_5fvalid',['is_valid',['../class_legion_1_1_physical_region.html#a654c9f7f09e19b63b515458348b4a472',1,'Legion::PhysicalRegion']]],
   ['issue_5facquire',['issue_acquire',['../class_legion_1_1_runtime.html#a1d1a34f794934fda97473fcbd8de8c8c',1,'Legion::Runtime']]],
   ['issue_5fcopy_5foperation',['issue_copy_operation',['../class_legion_1_1_runtime.html#a44776b6543dac1f165b5148831e341b8',1,'Legion::Runtime::issue_copy_operation(Context ctx, const CopyLauncher &amp;launcher)'],['../class_legion_1_1_runtime.html#a986853cf7b7747c747f664d04e05a4dd',1,'Legion::Runtime::issue_copy_operation(Context ctx, const IndexCopyLauncher &amp;launcher)']]],
   ['issue_5fexecution_5ffence',['issue_execution_fence',['../class_legion_1_1_runtime.html#aeea56eeac33caa8b05c1e82cb36c3159',1,'Legion::Runtime']]],

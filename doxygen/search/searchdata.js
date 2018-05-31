aec88273a43442f5cc4bcbf6b23da51330db2ccd
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-05-31 15:37:13 -0700

    Deploy Thu May 31 15:37:12 PDT 2018.

--- doxygen/search/searchdata.js
+++ doxygen/search/searchdata.js
@@ -1,36 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghilmnprstuw",
-  1: "acdfgilmprst",
+  0: "cl",
+  1: "cl",
   2: "l",
   3: "l",
-  4: "abcdefghilmprsuw",
-  5: "fhinprt",
-  6: "l",
-  7: "dl"
+  4: "l"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "pages"
+  2: "files",
+  3: "functions",
+  4: "typedefs"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Pages"
+  2: "Files",
+  3: "Functions",
+  4: "Typedefs"
 };
 

89dff14d78cb4088d298963a95bcb2b507f808c5
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-05-31 15:46:41 -0700

    Deploy Thu May 31 15:46:39 PDT 2018.

--- doxygen/search/classes_0.js
+++ doxygen/search/classes_0.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['contextinterface',['ContextInterface',['../class_legion_1_1_internal_1_1_context_interface.html',1,'Legion::Internal']]]
+  ['acquire',['Acquire',['../class_legion_1_1_acquire.html',1,'Legion']]],
+  ['acquirelauncher',['AcquireLauncher',['../struct_legion_1_1_acquire_launcher.html',1,'Legion']]],
+  ['argumentmap',['ArgumentMap',['../class_legion_1_1_argument_map.html',1,'Legion']]],
+  ['attachlauncher',['AttachLauncher',['../struct_legion_1_1_attach_launcher.html',1,'Legion']]]
 ];

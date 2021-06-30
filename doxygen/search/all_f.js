e3bfec42abb801f955eef202740361decf02cfa7
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-06-30 10:09:46 -0700

    Deploy Wed Jun 30 10:09:43 PDT 2021.

--- doxygen/search/all_f.js
+++ doxygen/search/all_f.js
@@ -5,6 +5,7 @@ var searchData=
   ['taskargument',['TaskArgument',['../class_legion_1_1_task_argument.html',1,'Legion']]],
   ['taskconfigoptions',['TaskConfigOptions',['../struct_legion_1_1_task_config_options.html',1,'Legion']]],
   ['tasklauncher',['TaskLauncher',['../struct_legion_1_1_task_launcher.html',1,'Legion']]],
+  ['taskmut',['TaskMut',['../class_legion_1_1_task_mut.html',1,'Legion']]],
   ['taskvariantregistrar',['TaskVariantRegistrar',['../struct_legion_1_1_task_variant_registrar.html',1,'Legion']]],
   ['timinglauncher',['TimingLauncher',['../struct_legion_1_1_timing_launcher.html',1,'Legion']]],
   ['tlbitmask',['TLBitMask',['../class_t_l_bit_mask.html',1,'']]],
